91efd40f0d8b8f47459c0c0f28833aa8648f3904
Patrick Hobusch <patrick.hobusch@gmail.com>
2021-03-12 15:45:26 +0800

    Try to deploy Helm chart with Terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "my-context"
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.0.3"
+    }
+  }
 }
 
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "example"
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "nginx-ingress"
+  version    = "3.24.0"
+  namespace  = "kube-system"
+
+  values = [
+    file("helm/nginx-ingress/values.yml")
+  ]
 }
 
