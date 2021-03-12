94830d6463a4af51b5e50d09ab2a35bc19aa6090
Patrick Hobusch <patrick.hobusch@gmail.com>
2021-03-12 15:32:38 +0800

    Prepare for Terraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "example"
+  }
+}
+
